135b9174b03037f281a09399fbbe62a1329ebec2
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-08-31 15:25:14 +0300

    veri modeli

---
+++ BilgeKafe.Data/Siparis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class Siparis
+    {
+        public int MasaNo { get; set; }
+
+        public DateTime? AcilisZamani { get; set; } = DateTime.Now;
+
+        public DateTime? KapanisZamani { get; set; }
+
+        public SiparisDurum Durum { get; set; } = SiparisDurum.Aktif;
+
+        public decimal OdenenTutar { get; set; }
+
+        public List<SiparisDetay> SiparisDetaylar { get; set; } = new List<SiparisDetay>();
+
+        public string ToplamTutarTL => $"{ToplamTutar():n2}₺";
+
+        public decimal ToplamTutar() => SiparisDetaylar.Sum(sd => sd.Tutar());
+        //public decimal ToplamTutar()
+        //{
+        //    decimal toplam = 0;
+        //    foreach (SiparisDetay detay in SiparisDetaylar)
+        //        toplam += detay.Tutar();
+        //    return toplam;
+        //}
+    }
+}
